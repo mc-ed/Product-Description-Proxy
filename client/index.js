25004f48be3cfb1868f1e56572c3d6f6e0adcd0b
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-07-03 09:05:05 -0500

    boilerplate

---
+++ client/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        string: 'This is the App!',
+        message: ''
+     };
+  }
+
+  componentDidMount() {
+    window.addEventListener("message", receiveMessage.bind(this), 'http://127.0.0.1:3050');
+    // console.log('my frames:', window.frames)
+    const context = this;
+    function receiveMessage(event) {
+      console.log(event)
+      if (event.origin === "http://127.0.0.1:3050" && typeof event.data === 'string')
+        console.log(event.data);
+        console.log(context)
+        context.settingState([event.origin], event.data)
+        return;
+      }
+  }
+
+  settingState(key, value) {
+    this.setState({[key] : value})
+  }
+
+  render() {
+    const styleProDesc = {
+      border: 'none',
+      width: '1200px',
+      height: '300px'
+    }
+
+    return (
+      <div>
+        <p>{ this.state.string }</p>
+        <p>These are my props: { JSON.stringify(this.props) }</p>
+        <p>Message from my child iframe: { this.state.message }</p>
+        <iframe style={ styleProDesc } src="http://127.0.0.1:3050/"></iframe>
+      </div>
+    );
+  }
+}
+
+let domContainer = document.querySelector('#app');
+ReactDOM.render(<App />, domContainer);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'public')
+    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+}   
+};
