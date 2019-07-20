ab809218b38aebba1971c782e6d0000b6819f72d
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-07-20 01:55:15 -0500

    adding CSS transitions to launch page

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LoadingOverlay from 'react-loading-overlay';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,24 +8,38 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    setTimeout(()=>{
-      document.body.classList.add('noScroll');
-      document.querySelector("#search-banner > header > div > div > div > div.col-9 > div").classList.add('launchSearchBar')
-      document.querySelector("#search-banner > header > div > div > div > div.col-9 > div > form > div > div.container.dropdown-menu").classList.add('launchDropDown')
-      document.querySelector("#search-banner > header > div > div > div > div.col-3").classList.add('launchDepartments');
-    },300)
+    document.body.classList.add('noScroll');
+    window.onload = () => {
+      setTimeout(()=>{
+        document.querySelector("#search-banner > header > div > div > div > div.col-3").classList.add('launchDepartments')
+        document.querySelector("#search-banner > header > div > div > div > div.col-9 > div").classList.add('launchSearchBar')
+        document.querySelector("#search-banner > header > div > div > div > div.col-9 > div > form > div > div.container.dropdown-menu").classList.add('launchDropDown')
+        
+      },500)
+      console.log('window loaded')
+  }
     console.log('Proxy Script Loaded!')
     window.addEventListener('product', () => {
+      document.getElementById('proxyContainer').classList.add('hideIT');
+      document.getElementById('search-banner').classList.add('hideIT');
       setTimeout(()=>{
-        document.getElementById('proxyContainer').style.display = 'none';
-        document.getElementById('overviewContainer').style.display = 'block';
-        document.getElementById('descriptionContainer').style.display = 'block';
-        document.getElementById('carouselContainer').style.display = 'block';
+        document.getElementById('proxyContainer').classList.add('removeIT');
+        document.getElementById('overviewContainer').classList.remove('removeIT');
+        document.getElementById('bottomOfPage').classList.remove('removeIT');
+        document.getElementById('descriptionContainer').classList.remove('removeIT');
+        document.getElementById('carouselContainer').classList.remove('removeIT');
         document.querySelector("#search-banner > header > div > div > div > div.col-9 > div").classList.remove('launchSearchBar')
         document.querySelector("#search-banner > header > div > div > div > div.col-9 > div > form > div > div.container.dropdown-menu").classList.remove('launchDropDown')
         document.querySelector("#search-banner > header > div > div > div > div.col-3").classList.remove('launchDepartments');
         document.body.classList.remove('noScroll');
-      },200)
+        setTimeout(() => {
+          document.getElementById('search-banner').classList.remove('hideIT');
+          document.getElementById('overviewContainer').classList.remove('hideIT');
+          document.getElementById('bottomOfPage').classList.remove('hideIT');
+          document.getElementById('descriptionContainer').classList.remove('hideIT');
+          document.getElementById('carouselContainer').classList.remove('hideIT');
+        },50)
+      },500)
     })
   }
 
@@ -32,9 +47,9 @@ class App extends React.Component {
   render() {
     return (
       <div id='hero'>
-        <div class="position-img-outer">
-          <div class="position-img-inner">
-            <img src="https://lowesproject.s3.amazonaws.com/banner-home-desk_v2.jpg" class="position-img" alt="img" style={{ height: "895px"}} />
+        <div className="position-img-outer">
+          <div className="position-img-inner">
+            <img src="https://lowesproject.s3.amazonaws.com/banner-home-desk_v2.jpg" className="position-img" alt="img" style={{ height: "895px"}} />
           </div>
           </div>
       </div>
